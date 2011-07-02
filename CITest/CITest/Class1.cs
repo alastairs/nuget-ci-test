d2d96d92b0e1b10c2ef6471363a550650820b618
Alastair Smith <github@alastairsmith.me.uk>
2011-07-02 22:32:34 +0100

    Checking in initial version: the packages DIRECTORY is explicitly ignored so we don't keep binaries in the repo.  Solution otherwise builds on my machine.

---
+++ CITest/CITest/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reactive.Linq;
+using System.Threading;
+
+namespace CITest
+{
+    public class Class1
+    {
+        // The point of this code is to exercise the build system. This is why it's rubbish.  
+        public void DoStuff()
+        {
+            // This sample cam from http://rxwiki.wikidot.com/101samples#toc1
+            // Observable comes from the Reactive Extensions, which is included via NuGet. If NuGet build steps fail, the project will not build.
+            var o = Observable.Start(() =>
+                                         {
+                                             Console.WriteLine("Calculating..."); 
+                                             Thread.Sleep(3000); 
+                                             Console.WriteLine("Done.");
+                                         });
+
+            o.First();
+        }
+
+        // NOTE: Confirmed build on my machine.
+    }
+}
